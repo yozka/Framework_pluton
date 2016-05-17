e5453701e2fd94b4ca43b4308b0147285a53b471
yozka <yozka@tigraha.com>
2016-05-17 20:08:39 +0500

    Аналитика и билинг для андроида

--- Source/Platform/Android/Marketplace/fwMarketplace.cs
+++ Source/Platform/Android/Marketplace/fwMarketplace.cs
@@ -4,7 +4,15 @@ using System.Collections.Generic;
 
 namespace Pluton.SystemProgram.Devices
 {
-  
+
+    ///--------------------------------------------------------------------------------------
+    using Xamarin.InAppBilling;
+    using Xamarin.InAppBilling.Utilities;
+
+    using Android.App;
+    using Android.OS;
+    using Android.Content;
+    ///--------------------------------------------------------------------------------------
 
 
 
@@ -33,8 +41,17 @@ namespace Pluton.SystemProgram.Devices
 
         private int mPurchase = 0; //количество покупок
 
-        private EState mState = EState.none; //текущее состояние магазина
 
+
+        private InAppBillingServiceConnection mServiceConnection = null; //сервис магазина
+        private readonly Dictionary<string, Product> mProducts = new Dictionary<string, Product>();
+        private readonly Dictionary<string, Purchase> mPurchases = new Dictionary<string, Purchase>();
+
+        private bool mError = false; //сама ошибка
+        private readonly Dictionary<string, string> mLastError = new Dictionary<string, string>(); //описание ошибки
+
+
+        private EState mState = EState.none; //текущее состояние магазина
         ///--------------------------------------------------------------------------------------
         private enum EState
         {
@@ -51,7 +68,7 @@ namespace Pluton.SystemProgram.Devices
 
 
 
-        ///=====================================================================================
+         ///=====================================================================================
         ///
         /// <summary>
         /// Constructor
@@ -60,7 +77,6 @@ namespace Pluton.SystemProgram.Devices
         ///--------------------------------------------------------------------------------------
         public AMarketplace()
         {
-           
         }
         ///--------------------------------------------------------------------------------------
 
@@ -69,8 +85,153 @@ namespace Pluton.SystemProgram.Devices
 
 
 
+        ///=====================================================================================
+        ///
+        /// <summary>
+        /// инциализация магазина
+        /// </summary>
+        /// 
+        ///--------------------------------------------------------------------------------------
+        public void create(Activity activity, string publicKey)
+        {
+             mServiceConnection = new InAppBillingServiceConnection(activity, publicKey);
+      
+   /* чото этот код не работает
+            Intent serviceIntent = new Intent("com.android.vending.billing.InAppBillingService.BIND");
+            serviceIntent.SetPackage("com.android.vending");
+
+            //GetApplicationContext().bindService(serviceIntent, connection, Context.MODE_PRIVATE);
+            Context bbs = activity.ApplicationContext;
+
+            bool rt =  bbs.BindService(serviceIntent, mServiceConnection, Bind.AutoCreate);
+ */
+
+
+
+            mServiceConnection.OnConnected += () =>
+            {
+                mError = false;
+                
+                // Attach to the various error handlers to report issues
+                mServiceConnection.BillingHandler.OnGetProductsError += (int responseCode, Bundle ownedItems) =>
+                {
+                    Console.WriteLine("Error getting products");
+                };
+
+                mServiceConnection.BillingHandler.OnInvalidOwnedItemsBundleReturned += (Bundle ownedItems) =>
+                {
+                    Console.WriteLine("Invalid owned items bundle returned");
+                };
+
+                mServiceConnection.BillingHandler.OnProductPurchasedError += (int responseCode, string sku) =>
+                {
+                    Console.WriteLine("Error purchasing item {0}", sku);
+                    mPurchases[sku] = null;
+                    mLastError[sku] = "Error purchasing";
+                    mError = true;
+                };
+
+                mServiceConnection.BillingHandler.OnPurchaseConsumedError += (int responseCode, string token) =>
+                {
+                    Console.WriteLine("Error consuming previous purchase");
+                };
+
+                mServiceConnection.BillingHandler.InAppBillingProcesingError += (message) =>
+                {
+                    Console.WriteLine("In app billing processing error {0}", message);
+                };
+
+
+
+                mServiceConnection.BillingHandler.BuyProductError += (int responseCode, string sku) =>
+                {
+                    Console.WriteLine("In app billing processing error ");
+                    mLastError[sku] = "In app processing error";
+                    mError = true;
+                };
+
+                mServiceConnection.BillingHandler.OnUserCanceled += () =>
+                {
+                    Console.WriteLine("OnUserCanceled");
+                };
+
+
+                mServiceConnection.BillingHandler.OnProductPurchased += (int response, Purchase purchase, string purchaseData, string purchaseSignature) =>
+                {
+                    if (purchase == null)
+                    {
+                        return;
+                    }
+                    mPurchases[purchase.ProductId] = purchase;
+                    mLastError.Remove(purchase.ProductId);
+                    mError = false;
+                };
+
+                loadProducts();
+            };
+
+
+            mServiceConnection.OnInAppBillingError += slot_billingError;
+        }
+        ///--------------------------------------------------------------------------------------
+
+
+
+
+
 
          ///=====================================================================================
+        ///
+        /// <summary>
+        /// ошибка билинга
+        /// </summary>
+        /// 
+        ///--------------------------------------------------------------------------------------
+        private void slot_billingError(InAppBillingErrorType error, string message)
+        {
+            setError(message);
+        }
+        ///--------------------------------------------------------------------------------------
+
+
+
+
+
+
+         ///=====================================================================================
+        ///
+        /// <summary>
+        /// загрузка списка продуктов
+        /// </summary>
+        /// 
+        ///--------------------------------------------------------------------------------------
+        public async void loadProducts()
+        {
+            mError = false;
+            IList<Product> list = await mServiceConnection.BillingHandler.QueryInventoryAsync(mProductID, ItemType.Product);
+            if (list == null)
+            {
+                //ошибка
+                setError("Error market");
+                return;
+            }
+    
+            //загрузили все нормлаьно
+            foreach (var obj in list)
+            {
+                mProducts[obj.ProductId] = obj;
+            }
+
+            mState = EState.completed;
+        }
+        ///--------------------------------------------------------------------------------------
+
+
+
+
+
+
+        ///=====================================================================================
         ///
         /// <summary>
         /// инцилизация списка продуктов
@@ -104,7 +265,7 @@ namespace Pluton.SystemProgram.Devices
 
 
 
-        ///=====================================================================================
+         ///=====================================================================================
         ///
         /// <summary>
         /// загрузка цены и описание товаров
@@ -113,23 +274,13 @@ namespace Pluton.SystemProgram.Devices
         ///--------------------------------------------------------------------------------------
         private void updateStore()
         {
- 
-        }
-        ///--------------------------------------------------------------------------------------
+            if (mState != EState.none)
+            {
+                return;
+            }
 
-
-
-
-         ///=====================================================================================
-        ///
-        /// <summary>
-        /// загрузка цены прошла успешно
-        /// </summary>
-        /// 
-        ///--------------------------------------------------------------------------------------
-        private void slot_productsCompleted ()
-        {
-            mState = EState.completed;
+            mState = EState.loading;
+            mServiceConnection.Connect();
         }
         ///--------------------------------------------------------------------------------------
 
@@ -137,20 +288,7 @@ namespace Pluton.SystemProgram.Devices
 
 
 
-         ///=====================================================================================
-        ///
-        /// <summary>
-        /// ошибка при загрузке цены
-        /// </summary>
-        /// 
-        ///--------------------------------------------------------------------------------------
-        private void slot_productsFailed()
-        {
-            mState = EState.none;
-        }
-        ///--------------------------------------------------------------------------------------
-
-
+      
 
 
 
@@ -181,6 +319,16 @@ namespace Pluton.SystemProgram.Devices
         ///--------------------------------------------------------------------------------------
         public bool isProductBuy(string productID)
         {
+            if (mServiceConnection.BillingHandler == null)
+            {
+                return false;
+            }
+
+            if (mPurchases.ContainsKey(productID))
+            {
+                return true;
+            }
+
             return false;
         }
         ///--------------------------------------------------------------------------------------
@@ -202,6 +350,24 @@ namespace Pluton.SystemProgram.Devices
         ///--------------------------------------------------------------------------------------
         public bool productCompleted(string productID)
         {
+            if (!mPurchases.ContainsKey(productID))
+            {
+                return false;
+            }
+
+            Purchase pur = mPurchases[productID];
+            mPurchases.Remove(productID);
+
+            if (pur == null)
+            {
+                //произошла ошибка при покупке
+                return false; 
+            }
+
+            if (mServiceConnection.BillingHandler.ConsumePurchase(pur))
+            {
+                return true;
+            }
             return false;
         }
         ///--------------------------------------------------------------------------------------
@@ -225,7 +391,12 @@ namespace Pluton.SystemProgram.Devices
         public string getDescription(string productID)
         {
             updateStore();
-            return "test";
+            if (mProducts.ContainsKey(productID))
+            {
+                return mProducts[productID].Title;
+
+            }
+            return null;
         }
         ///--------------------------------------------------------------------------------------
 
@@ -245,7 +416,17 @@ namespace Pluton.SystemProgram.Devices
         public string getName(string productID)
         {
             updateStore();
-            return "test";
+            if (mProducts.ContainsKey(productID))
+            {
+                return mProducts[productID].Description;
+
+            }
+            if (mState == EState.completed)
+            {
+                return "Error";
+            }
+
+            return null;
         }
         ///--------------------------------------------------------------------------------------
 
@@ -264,7 +445,18 @@ namespace Pluton.SystemProgram.Devices
         public string getFormattedPrice(string productID)
         {
             updateStore();
-            return "0.0rub";
+            if (mProducts.ContainsKey(productID))
+            {
+                string price = mProducts[productID].Price;
+                price = price.Replace(" \u20bd", " руб.");
+                return price;
+            }
+
+            if (mState == EState.completed)
+            {
+                return "...";
+            }
+            return null;
         }
         ///--------------------------------------------------------------------------------------
 
@@ -283,8 +475,20 @@ namespace Pluton.SystemProgram.Devices
         ///--------------------------------------------------------------------------------------
         public bool productBuy(string productID)
         {
-          
+            if (mState != EState.completed)
+            {
+                return false;
+            }
 
+            if (!mProducts.ContainsKey(productID))
+            {
+                return false;
+            }
+
+            mPurchases.Remove(productID);
+            mError = false;
+            var product = mProducts[productID];
+            mServiceConnection.BillingHandler.BuyProduct(product);
             return true;
         }
         ///--------------------------------------------------------------------------------------
@@ -294,10 +498,25 @@ namespace Pluton.SystemProgram.Devices
 
 
 
-
-
-
          ///=====================================================================================
+        ///
+        /// <summary>
+        /// установка ошибки
+        /// </summary>
+        /// 
+        ///--------------------------------------------------------------------------------------
+        private void setError(string message)
+        {
+            mError = true;
+            mLastError["all"] = message;
+            mState = EState.none;
+        }
+        ///--------------------------------------------------------------------------------------
+
+
+
+
+        ///=====================================================================================
         ///
         /// <summary>
         /// проверка, есть ошибка или нет
@@ -306,7 +525,7 @@ namespace Pluton.SystemProgram.Devices
         ///--------------------------------------------------------------------------------------
         public bool isLastError(string productID)
         {
-            return false;
+            return mError;
         }
         ///--------------------------------------------------------------------------------------
 
@@ -322,7 +541,11 @@ namespace Pluton.SystemProgram.Devices
         ///--------------------------------------------------------------------------------------
         public string getLastError(string productID)
         {
-            return null;
+            if (mLastError.ContainsKey(productID))
+            {
+                return mLastError[productID];
+            }
+            return "Error: " + productID;
         }
         ///--------------------------------------------------------------------------------------
 
